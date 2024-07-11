c7d276c3c5ec5f5ab786eb650d2f5d21e039778a
SagarLalwani1 <sagar.l.lalwani@gmail.com>
2024-07-11 05:56:30 +0530

    Some New

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { dialogueData, scaleFactor } from "./constants";
-import { k } from "./kaboomCtx";
-import { displayDialogue, setCamScale } from "./utils";
+import { dialogueData, scaleFactor } from "./constants.js";
+import { k } from "./kaboomCtx.js";
+import { displayDialogue, setCamScale } from "./utils.js";
 
-k.loadSprite("spritesheet","./spritesheet.png", {
+k.loadSprite("spritesheet","./public/spritesheet.png", {
     sliceX:39,
     sliceY: 31,
     anims:{
@@ -15,12 +15,12 @@ k.loadSprite("spritesheet","./spritesheet.png", {
     }
 })
 
-k.loadSprite("map", "./map.png")
+k.loadSprite("map", "./public/map.png")
 
 k.setBackground(k.Color.fromHex("#311047"))
 
 k.scene("main", async () => {
-    const mapData = await (await fetch("./map.json")).json()
+    const mapData = await (await fetch("./public/map.json")).json()
     const layers = mapData.layers
 
     const map = k.add([
